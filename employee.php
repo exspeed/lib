34a838d1d9827bcbf0a7aee8739addf5248135bc
Atheng <andrewheng2@gmail.com>
2015-12-01 14:00:13 -0700

    add employee and updated everything else

---
+++ employee.php
@@ -0,0 +1,163 @@
+<?php
+$db = 'library';
+$host = 'localhost';
+$port = 8889;
+
+$con = mysqli_connect ( $host, 'root', 'root', $db ) or die ( "some error" );
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.error {
+	color: #FF0000
+}
+</style>
+<title>Employee</title>
+</head>
+<body>
+
+	<h1>Employee</h1>
+	<h3>Add Books</h3>
+	<form name="addbooks"
+		action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
+		method="post">
+		title: <input type="text" name="title" /> <span class="error"> <?php $titleErr = ""; echo $titleErr;?></span>
+		<br> author:<select name="author"> <?php
+		$strSQL = "SELECT name FROM author";
+		$query = mysqli_query ( $con, $strSQL );
+		while ( $result = mysqli_fetch_assoc ( $query ) ) {
+			echo '<option value=' . '"' . $result ['name'] . '"' . '>' . $result ['name'] . '</option>';
+		}
+		?></select><br> publisher: <select name="publisher">
+			<?php
+			$strSQL = "SELECT name FROM publisher";
+			$query = mysqli_query ( $con, $strSQL );
+			while ( $result = mysqli_fetch_assoc ( $query ) ) {
+				echo '<option value=' . '"' . $result ['name'] . '"' . '>' . $result ['name'] . '</option>';
+			}
+			?>
+			</select> <input type="submit" name="addbook" value="add" />
+	</form>
+	<br>
+</body>
+
+
+<?php
+$authorErr = $publisherErr = $checkoutErr = "";
+if ($_SERVER ["REQUEST_METHOD"] == "POST") {
+	if (! empty ( $_POST ['addbook'] )) {
+		$title = $_POST ['title'];
+		if ($title == "") {
+			echo 'title can\'t be null';
+		} else {
+			$author = $_POST ['author'];
+			$publisher = $_POST ['publisher'];
+			$strSQL = "INSERT INTO book (title, author, publisher) VALUES ('$title', '$author', '$publisher')";
+			$result = mysqli_query ( $con, $strSQL );
+			if (! $result) {
+				echo 'book entry failed';
+			} else {
+				echo 'new book added';
+			}
+		}
+	}
+	
+	if (! empty ( $_POST ['addauthor'] )) {
+		$author = $_POST ['name'];
+		if ($authorErr == '') {
+			$authorErr = "field can't be empty";
+		} else {
+			$strSQL = "INSERT INTO author (name) VALUES ('$author')";
+			$result = mysqli_query ( $con, $strSQL );
+			if (! $result) {
+				$authorErr = "No duplicate entries";
+			}
+		}
+	}
+	if (! empty ( $_POST ['addpublisher'] )) {
+		$publisher = $_POST ['name'];
+		if ($publisher == '') {
+			$publisherErr = "field can't be empty";
+		} else {
+			$strSQL = "INSERT INTO publisher (name) VALUES ('$publisher')";
+			$result = mysqli_query ( $con, $strSQL );
+			if (! $result) {
+				$publisherErr = "No duplicate entries";
+			}
+		}
+	}
+	
+	if (! empty ( $_POST ['checkout'] )) {
+		$memberID = $_POST ['memberid'];
+		$book_isbn = $_POST ['bookisbn'];
+		
+		$strSQL = "SELECT available FROM book WHERE isbn = $book_isbn";
+		$query = mysqli_query ( $con, $strSQL );
+		if (! $query) {
+			echo mysqli_error ( $con );
+			exit ();
+		}
+		while ( $result = mysqli_fetch_array ( $query ) ) {
+			if ($result ['available'] == 1) {
+				$today = date ( "Y-m-d" );
+				$date = date ( 'Y-m-d', strtotime ( $today . '+ 14 days' ) );
+				$strSQL = "INSERT INTO memberborrowsbook (memberID, isbn, dueDate)
+				VALUES ('$memberID', '$book_isbn', '$date')";
+				
+				$result = mysqli_query ( $con, $strSQL );
+				
+				$strSQL = "UPDATE book SET available ='0' WHERE isbn= $book_isbn";
+				$result = mysqli_query($con, $strSQL);
+			}
+		}
+	}
+	
+	if(! empty($_POST['return'])){
+		$memberID = $_POST['memberid'];
+		$book_isbn = $_POST['bookisbn'];
+		
+		$strSQL = "DELETE FROM memberborrowsbook WHERE 
+		memberID = $memberID AND isbn = $book_isbn";
+		$query = mysqli_query($con, $strSQL);
+		if (! $query) {
+			echo mysqli_error ( $con );
+		}
+		
+		$strSQL = "UPDATE book SET available = '1' WHERE isbn = $book_isbn";
+		$query = mysqli_query($con, $strSQL);
+		
+	}
+	
+}
+?>
+
+<h3>Add Authors</h3>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
+	method="post">
+	Author: <input type="text" name="name" /> <input type="submit"
+		name="addauthor" value="add" /> <br> <?php echo $authorErr; ?>
+</form>
+<h3>Add Publisher</h3>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
+	method="post">
+	Publisher: <input type="text" name="name" /> <input type="submit"
+		name="addpublisher" value="add" /> <br> <?php echo $publisherErr; ?>	
+</form>
+<h3>Member Checks Out Book</h3>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
+	method="post">
+	MemberID: <input type="text" name="memberid" /><br> Book ISBN: <input
+		type="text" name="bookisbn" /> <br> <input type="submit"
+		name="checkout" value="checkout" /> <br> <?php echo $checkoutErr; ?>	
+</form>
+
+<h3>Member Returns Book</h3>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
+	method="post">
+	MemberID: <input type="text" name="memberid" /><br> Book ISBN: <input
+		type="text" name="bookisbn" /> <br> <input type="submit"
+		name="return" value="return" /> <br> <?php echo $checkoutErr; ?>	
+</form>
+</html>
--- login.php
+++ login.php
@@ -1,60 +1,128 @@
 <?php
-   $user = 'root';
-   $password = 'root';
-   $db = 'library';
-   $host = 'localhost';
-   $port = 8889;
+$user = 'root';
+$password = 'root';
+$db = 'library';
+$host = 'localhost';
+$port = 8889;
 
-   $con = mysqli_connect($host, $user,$password, $db) or die("some error");
-   $strSQL = "SELECT * FROM member INNER JOIN  memberaddress ON memberaddress.memberID = member.memberID";
+$con = mysqli_connect ( $host, $user, $password, $db ) or die ( "some error" );
+$strSQL = "SELECT * FROM member INNER JOIN  memberaddress ON memberaddress.memberID = member.memberID";
 
-   // checks if username and password is in the db
-   $goback = True;   
-  $memberID = $street = $number = $postalCode = "";
-   $fname= $lname=$fines= $totalBorrowed = "";
+// checks if username and password is in the db
+$goback = True;
+$memberID = $street = $number = $postalCode = "";
+$fname = $lname = $fines = $totalBorrowed = "";
 
-   $query = mysqli_query($con,$strSQL);
-/*   if(!$query){
-   echo mysqli_error($con);
-   exit();
-   }
+$query = mysqli_query ( $con, $strSQL );
 
-   good debugging code :D
-*/
-   while($result = mysqli_fetch_array($query)){
-   if($result['username'] == $_POST["username"] && $result['password'] == $_POST["password"]){
-   $goback = False;
-   $memberID = $result['memberID'];
-   $street =  $result['street'];
-   $number =  $result['number'];
-   $postalCode =  $result['postalCode'];
-   $fname =  $result['fname'];
-   $lname =  $result['lname'];
-   $fines =  $result['fines'];
-   $totalBorrowed =  $result['totalBorrowed'];
+while ( $result = mysqli_fetch_array ( $query ) ) {
+	if ($result ['username'] == $_POST ["username"] && $result ['password'] == $_POST ["password"]) {
+		$goback = False;
+		$memberID = $result ['memberID'];
+		$street = $result ['street'];
+		$number = $result ['number'];
+		$postalCode = $result ['postalCode'];
+		$fname = $result ['fname'];
+		$lname = $result ['lname'];
+		$fines = $result ['fines'];
+		$strSQL = "SELECT COUNT(*) as total FROM memberborrowsbook WHERE memberID= $memberID";
+		$query = mysqli_query ( $con, $strSQL );
+		$data = mysqli_fetch_assoc ( $query );
+		$totalBorrowed = $data ['total'];
+	}
+}
 
-   }
-   }
+if ($goback) {
+	header ( 'Location: index.php?incorrect=True' );
+}
 
-   if($goback){
-   header('Location: index.php?incorrect=True');
-   }
 ?>
 
 <!DOCTYPE html>
 <html>
-  <body>
+<body>
 
-    <h1>Account</h1>
-    <h2>Welcome <?php echo "$fname  $lname" ?></h2>
-    
-    <h3>Your Address: <?php echo "$number $street $postalCode";?><br>
+	<h1>Account</h1>
+	<h2>Welcome <?php echo "$fname  $lname" ?></h2>
+
+	<h3>Your Address: <?php echo "$number $street $postalCode";?><br>
       You owe us: <?php echo $fines; ?> <br>
       Number of books borrowed: <?php echo $totalBorrowed; ?><br>
       MemberID is <?php echo $memberID?>
     </h3>
-    
 
 
-  </body>
+
+</body>
+<style>
+table, th, td {
+	border: 1px solid black;
+	border-collapse: collapse;
+}
+
+th, td {
+	padding: 5px;
+}
+</style>
+<h3>Your Books</h3>
+	<?php
+	$strSQL = "SELECT * 
+	FROM book INNER JOIN memberborrowsbook 
+	WHERE book.isbn = memberborrowsbook.isbn 
+		AND memberborrowsbook.memberid = $memberID
+		ORDER BY book.title";
+	$query = mysqli_query ( $con, $strSQL );
+	if (! $query) {
+		echo "your book error " . mysqli_error ( $con );
+		exit ();
+	}
+	echo "<table style=\"width:100 %\">";
+	echo "<tr><th>Title</th><th>Author</th><th>Publisher</th><th>Due Date</th></tr>";
+	while ( $result = mysqli_fetch_array ( $query ) ) {
+		echo '<tr>';
+		echo '<td>' . $result ['title'] . '</td>';
+		echo '<td>' . $result ['author'] . '</td>';
+		echo '<td>' . $result ['publisher'] . '</td>';
+		echo '<td>' . $result ['dueDate'] . '</td>';
+		echo '</tr>';
+	}
+	echo '</table>';
+	?>
+<h3>Available Books</h3>
+<form action="" method="post">
+	Search: <input type="text" name="name" /> By: <input type="radio"
+		name="search" value="title" checked="checked"> Title <input
+		type="radio" name="search" value="author">Author <input type="hidden"
+		name="username" value="<?php echo $_POST['username']?>"> <input
+		type="hidden" name="password" value="<?php echo $_POST['password']?>">
+	<input type="submit" name="submit" value="Go" /><br> <br>
+</form>
+
+  <?php
+		if (! empty ( $_POST ['submit'] )) {
+			$strSQL = "";
+			if($_POST['name'] == ''){
+				$strSQL = "SELECT * FROM book WHERE available = 1 ORDER BY title";}
+			else if ($_POST['search'] == 'title') {
+				$title = $_POST['name'];
+				$strSQL = "SELECT * FROM book WHERE available = 1 AND
+				title = '$title'";
+			} else {
+				$author = $_POST['name'];
+				$strSQL = "SELECT * FROM book WHERE available = 1 AND
+				author = '$author'";
+			}
+			$query = mysqli_query ( $con, $strSQL );
+			echo "<table style=\"width:100 %\">";
+			echo "<tr><th>Title</th><th>Author</th><th>Publisher</th></tr>";
+			while ( $result = mysqli_fetch_array ( $query ) ) {
+				echo '<tr>';
+				echo '<td>' . $result ['title'] . '</td>';
+				echo '<td>' . $result ['author'] . '</td>';
+				echo '<td>' . $result ['publisher'] . '</td>';
+				echo '</tr>';
+			}
+			echo '</table>';
+		}
+		?>
 </html>
